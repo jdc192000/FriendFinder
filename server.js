700f50c2503418cac70532f685b58141017371ca
John Coleman <johncoleman_2@hotmail.com>
2018-04-19 10:36:48 -0500

    initial load

---
+++ server.js
@@ -0,0 +1,19 @@
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+app.listen(process.env.PORT || PORT, function() {
+  console.log("App listening on PORT" + PORT);
+ });
+
+ 
