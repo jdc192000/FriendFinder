58dd22d1e3e06dfcb4d6d1ae7a6301f9b87c205b
John Coleman <johncoleman_2@hotmail.com>
2018-04-19 10:59:10 -0500

    more changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 require('./app/routing/api-routes.js')(app);
 require('./app/routing/html-routes.js')(app);
 
-app.listen(process.env.PORT || PORT, function() {
+app.listen(PORT, function() {
   console.log("App listening on PORT" + PORT);
  });
 
